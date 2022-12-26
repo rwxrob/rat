17f0c135a1968490340e6fe102e439929af4a579
Rob Muhlestein <rob@rwx.gg>
2022-12-26 16:57:44 -0500

    Working commit

--- rat.go
+++ rat.go
@@ -1,24 +1,124 @@
 /*
 Package rat is Inspired by Bryan Ford's PEG packrat parser paper and is
-PEGN aware (without depending on any external PEGN module). This
-no-scanner-required parser is a happy medium between maintainability,
-simplicity, and performance. Developers can dial up performance by using
-only Checks, or convenience and simplicity with Grammars and Pack
-structures enabling simple code generation and dynamic PEGN expression
-parsing.
+PEGN (version 2023-01) aware (without dependencies). The packrat
+approach does not require a scanner. It is a happy medium between
+maintainability and performance. Developers can use rat in
+different ways depending on the performance requirements:
 
-Consider github.com/rwxrob/pegn-go when a full pegn.Grammar is required
-(which is an instance of rat.Grammar).
+* Generate compilable Go code from PEGN or rat structs with rat.Pack(in).Gen(w)
+* Dynamically create new packrat grammars from PEGN or rat structs with rat.Pack(in)
+* Dynamically parse from ad-hoc grammar with rat.Parse(pegn,in)
+* Code grammars directly using shareable rat.Rule libraries
 
 */
 package rat
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 	"sync"
 )
+
+// ------------------------------- Pack -------------------------------
+
+// Pack returns a new Grammar from a variety of different input types.
+// All arguments are evaluated by type with the most common types first
+// in the type switch. For fastest grammar creation pass only predefined
+// Rules. For the slowest creation (albeit more convenient) pass
+// PEGN(string) types. Aggregate Grammars can be created from others as
+// well by importing other Grammar types. All types are parsed and
+// cached using the highly concurrent and optimized sync.Map type (see
+// Grammar). Once Packed, the returned Grammar can be used to
+// dynamically generate compilable Go code (see Gen).
+//
+// The order of type evaluation and caching is prioritized as
+// follows:
+//
+//     Rule     <as is>
+//     string   <inferred Lit>
+//     Lit      "foo\n" -> ('foo' LF)
+//     Seq      (rule1 rule2)
+//     One      (rule1 / rule2)
+//     Any      rune{n}
+//     Opt      rule?
+//     Min0     rule*
+//     Min1     rule+
+//     Rep      rule{n}
+//     Look     (&rule)
+//     Not      (!rule)
+//     MMax     rule{min,max}
+//     Min      rule{min,}
+//     Max      rule{0,max}
+//     Rng      [a-z]
+//     Set      ('a' / 'b' / 'c')
+//     NSet     !('a' / 'b' / 'c')
+//     To       (rune* &rule)
+//     Toi      (rune* rule)
+//     Cap      <capture as sub>
+//     Tag      <=Foo 'foo' > or Foo <- 'foo'
+//     Grammar  <rules imported, overwrite existing>
+//     PEGN     <dynamically parsed into rules and cached>
+//
+// Pointers to these types are not accepted (but the pointer to the underlying Check
+// function always is).
+func Pack(in ...any) *Grammar {
+	g := new(Grammar)
+	for _, it := range in {
+		switch v := it.(type) {
+		case Rule:
+			g.Store(v.Text, v)
+		case string, Lit:
+			g.Lit(string(v))
+		case Seq:
+			g.Seq(v.Rules...)
+		case One:
+			g.One(v.Rules...)
+		case Any:
+			g.Any(v)
+		case Opt:
+			g.Opt(v.Rules...)
+		case Min0:
+			g.Min0(v.Rules...)
+		case Min1:
+			g.Min1(v.Rules...)
+		case Rep:
+			g.Rep(v.N, v.Rules...)
+		case Look:
+			g.Look(v.Rules...)
+		case Not:
+			g.Not(v.Rules...)
+		case MMax:
+			g.MMax(v.Min, v.Max, v.Rules...)
+		case Min:
+			g.Min(v.Min, v.Rules...)
+		case Max:
+			g.Max(v.Max, v.Rules...)
+		case Rng:
+			g.Rng(v.Beg, v.End)
+		case Set:
+			g.Set(v)
+		case NSet:
+			g.NSet(v)
+		case To:
+			g.To(v.Rules...)
+		case Toi:
+			g.Toi(v.Rules...)
+		case Cap:
+			g.Cap(v.Rules...)
+		case Tag:
+			g.Tag(v.Tag, v.Rules...)
+		case *Grammar:
+			g.Import(v)
+		case Grammar:
+			g.Import(&v)
+		case PEGN:
+			g.PEGN(v)
+		}
+	}
+	return g
+}
 
 // ------------------------------ Grammar -----------------------------
 
@@ -35,17 +135,6 @@ import (
 // trees of Results against any buffered text data.
 type Grammar struct {
 	sync.Map
-}
-
-// NewGrammar takes zero or more Rules and returns a grammar
-// instantiated from them. Rules are expected to have unique Text field
-// values. If conflicting rule.Text values occur the last one wins.
-func NewGrammar(rules ...Rule) *Grammar {
-	g := new(Grammar)
-	for _, rule := range rules {
-		g.Store(rule.Text, rule)
-	}
-	return g
 }
 
 // Check calls Check on the rule specified by Text key. Sets result
@@ -83,8 +172,8 @@ func (g *Grammar) CheckString(ruletext string, rstr string, i int) Result {
 // combined from multiple packages. For best performance, Rules
 // should be created and used from a Grammar with proper caching.
 type Rule struct {
-	Text string
-	Check
+	Text  string
+	Check CheckFunc
 }
 
 func (r Rule) String() string {
@@ -94,24 +183,24 @@ func (r Rule) String() string {
 	return r.Text
 }
 
-// ------------------------------- Check ------------------------------
+// ----------------------------- CheckFunc ----------------------------
 
-// Check evaluates the []rune buffer at a specific position for
+// CheckFunc evaluates the []rune buffer at a specific position for
 // a specific grammar rule and should generally only be used from an
 // encapsulating Rule so that it has a Text identifier associated with
-// it. One or more Rules may, however, encapsulate the same Check
+// it. One or more Rules may, however, encapsulate the same CheckFunc
 // function.
 //
-// Check MUST return a Result indicating success or failure by setting
+// CheckFunc MUST return a Result indicating success or failure by setting
 // Err for failure. (Note that this is unlike many packrat designs that
 // return nil to indicate rule failure.)
 //
-// Check MUST advance the End to the farthest possible position
+// CheckFunc MUST advance the End to the farthest possible position
 // in the []rune slice before failure occurred providing an Err if
 // unable to match the entire rule. This allows for better recovery and
 // specific user-facing error messages and promotes succinct rule
 // development.
-type Check func(r []rune, i int) Result
+type CheckFunc func(r []rune, i int) Result
 
 // ------------------------------ Result ------------------------------
 
@@ -172,6 +261,23 @@ func (m Result) String() string {
 
 // Print is shortcut for fmt.Println(String).
 func (m Result) Print() { fmt.Println(m) }
+
+// Tagged does a depth first descent into the child sub-results (S)
+// adding anything with any of the matching tags to the returned slice.
+func (m Result) Tagged(tags ...string) []Results {
+	res := []Results{}
+	// TODO
+	return res
+}
+
+// TaggedExp does a depth first descent into the child sub-results (S)
+// returning all flattened results with a tag matching the passed
+// regular expression.
+func (m Result) TaggedExp(exp *regexp.Regexp) []Results {
+	res := []Results{}
+	// TODO
+	return res
+}
 
 // ------------------------------ ToPEGN ------------------------------
 
@@ -324,17 +430,10 @@ func (s One) String() string {
 }
 
 // One returns the results of the first rule to successfully match.
-// Equivalent to PEGN "(foo / bar)".
-func (c *Grammar) One(rules ...Rule) Rule {
+func (rules One) Rule() Rule {
 
 	rule := Rule{
 		Text: One(rules).String(),
-	}
-
-	if cached, has := c.Load(rule.Text); has {
-		if rule, ok := cached.(Rule); ok {
-			return rule
-		}
 	}
 
 	rule.Check = func(r []rune, i int) Result {
@@ -348,6 +447,11 @@ func (c *Grammar) One(rules ...Rule) Rule {
 		return Result{R: r, B: i, E: i, X: ErrExpected{rule}}
 	}
 
+	return rule
+}
+
+func (c *Grammar) One(rules ...Rule) Rule {
+	rule := One(rules).Rule()
 	c.Store(rule.Text, rule)
 	return rule
 }
@@ -357,49 +461,40 @@ func (c *Grammar) One(rules ...Rule) Rule {
 // Sequence as in "(foo bar)".
 type Seq []Rule
 
-func (s Seq) String() string {
-	str := "(" + s[0].Text
-	for _, v := range s[1:] {
+func (rules Seq) String() string {
+	str := "(" + rules[0].Text
+	for _, v := range rules[1:] {
 		str += " " + v.Text
 	}
 	return str + ")"
 }
 
-// Seq returns a new rule that is the sequential aggregation of all
-// rules passed to it stopping on the first to return an Err. Each
-// result is added as a Sub match along with the last failed match (if
-// any). Equivalent to PEGN "(foo bar)".
-func (c *Grammar) Seq(rules ...Rule) Rule {
-
-	rule := Rule{
+func (rules Seq) Rule() Rule {
+	return Rule{
 		Text: Seq(rules).String(),
-	}
-
-	if cached, has := c.Load(rule.Text); has {
-		if rule, ok := cached.(Rule); ok {
-			return rule
-		}
-	}
-
-	rule.Check = func(r []rune, i int) Result {
-		var err error
-		sub := []Result{}
-		start := i
-		for _, rule := range rules {
-			res := rule.Check(r, i)
-			i = res.E
-			if res.X != nil {
-				err = res.X
-				break
+		Check: func(r []rune, i int) Result {
+			var err error
+			sub := []Result{}
+			start := i
+			for _, rule := range rules {
+				res := rule.Check(r, i)
+				i = res.E
+				if res.X != nil {
+					err = res.X
+					break
+				}
+				sub = append(sub, res)
 			}
-			sub = append(sub, res)
-		}
-		if len(sub) == 0 {
-			return Result{R: r, B: start, E: i, X: err}
-		}
-		return Result{R: r, B: start, E: i, S: sub, X: err}
+			if len(sub) == 0 {
+				return Result{R: r, B: start, E: i, X: err}
+			}
+			return Result{R: r, B: start, E: i, S: sub, X: err}
+		},
 	}
+}
 
+func (c *Grammar) Seq(rules ...Rule) Rule {
+	rule := Seq(rules).Rule()
 	c.Store(rule.Text, rule)
 	return rule
 }
@@ -409,18 +504,24 @@ func (c *Grammar) Seq(rules ...Rule) Rule {
 // Optional as in "(foo bar)?".
 type Opt []Rule
 
-// Finite number (n) as in "(foo bar){n}".
-type N struct {
+// -------------------------------- Rep -------------------------------
+
+// Repeat n times as in "(foo bar){n}".
+type Rep struct {
 	N     int
 	Rules []Rule
 }
 
+// ------------------------------ MMax --------------------------------
+
 // Minimum (m) and maximum (n) as in "(foo bar){m,n}".
-type MinMax struct {
+type MMax struct {
 	Min   int
 	Max   int
 	Rules []Rule
 }
+
+// -------------------------------- Min -------------------------------
 
 // Minimum (m) as in "(foo bar){m,}".
 type Min struct {
@@ -428,44 +529,157 @@ type Min struct {
 	Rules []Rule
 }
 
+// -------------------------------- Max -------------------------------
+
 // Maximum (m) as in "(foo bar){0,m}".
 type Max struct {
 	Max   int
 	Rules []Rule
 }
 
+// ------------------------------- Min0 -------------------------------
+
 // Zero minimum as in "(foo bar)*".
 type Min0 []Rule
 
-// One minimum as in "(foo bar)+".
+// ------------------------------- Min1 -------------------------------
+
+// One minimum as in "(foo bar)+"
 type Min1 []Rule
 
-// Range between runes as in "[a-z]".
+// -------------------------------- Rng -------------------------------
+
+// Range between runes as in "[a-z]", "[x20-x34]".
 type Rng struct {
 	Beg rune
 	End rune
 }
 
+// -------------------------------- Set -------------------------------
+
+// Set from string as in "('a' / 'e' / 'i' / 'o' / 'u')".
+type Set string
+
+// ------------------------------- NSet -------------------------------
+
+// Inverted set from script as in "!('a' / 'e' / 'i' / 'o' / 'u')". Same
+// as "Not{Set(`aeiou`)}".
+type NSet string
+
+// -------------------------------- Cap -------------------------------
+
 // Capture as sub as in "< foo bar >".
 type Cap []Rule
 
-// Tagged capture as sub as in "<=tag foo bar >".
+// -------------------------------- Tag -------------------------------
+
+// Tagged capture as sub as in "Foo <- 'foo'" or "<=Foo 'foo' >".
 type Tag struct {
 	Tag   string
 	Rules []Rule
 }
 
+// ------------------------------- Look -------------------------------
+
 // Positive lookahead as in "&(foo bar)".
-type Pos []Rule
+type Look []Rule
+
+// -------------------------------- Not -------------------------------
 
 // Negative lookahead as in "!(foo bar)".
-type Neg []Rule
+type Not []Rule
+
+// -------------------------------- To --------------------------------
 
 // Up to as in "((!(foo bar) rune)*)".
 type To []Rule
 
+// -------------------------------- ToI -------------------------------
+
 // Up to inclusive as in "((!(foo bar) rune)*(foo bar))".
-type ToI []Rule
+type Toi []Rule
+
+// ------------------------------- PEGN -------------------------------
+
+// PEGN communicates to Pack that a string has specific meaning and PEGN
+// syntax, otherwise Pack assumes all strings as Lit types.
+type PEGN string
+
+func (p PEGN) String() string {
+	// TODO
+
+	return ""
+}
+
+func (p PEGN) Rule() Rule {
+	rule := Rule{
+		Text:  p.String(),
+		Check: Parse(p),
+	}
+	return rule
+}
+
+// PEGN parses the input (string, []byte, []rune, or PEGN) and returns
+// a single top level Rule covering the mini-grammar passed as an
+// argument.
+func (g *Grammar) PEGN(in any) Rule {
+	rule := PEGN(pegn).Rule()
+	c.Store(rule.Text, rule)
+	return rule
+}
+
+// ------------------------------- Check ------------------------------
+
+type textual interface {
+	PEGN | string | []rune | []byte
+}
+
+// Check takes a PEGN rule definition, dynamically compiles it, and
+// checks the input returning the Result. This function is slower than
+// other alternatives (like a regular expression without compilation),
+// but often more convenient. Tagging can be a convenient way to
+// retrieve the specific desired result quickly when dealing with
+// complex rule definitions. See Result.Tagged.
+func Check[T textual, I textual](pegnrule T, in M) Result {
+	rule := PEGN(pegnrule).Rule()
+	runes := []rune(string(in))
+	return rule.Check(runes, 0)
+}
+
+// ------------------------------ Import ------------------------------
+
+// Import imports the passed Grammar into the current Grammar
+// overwriting any conflicting keys.
+func (g Grammar) Import(a *Grammar) {
+	a.Range(func(k, v any) bool { g.Store(k, v); return true })
+}
+
+// -------------------------- Grammar.String --------------------------
+
+// String fulfills the fmt.Stringer interface by rendering a PEGN
+// grammar with incremental Rule names and keys as the Rule definitions.
+func (g Grammar) String() string {
+	// TODO eventually this should parse each of the rule keys breaking
+	// them down into each rule subcomponent and lookup that item from the
+	// grammar replacing that location with the invented Rule name. For
+	// example, given the following:
+	//
+	// Rule1 <- 'Foo' LF 'Bar' LF{2}
+	// Rule2 <- LF{2}
+	//
+	// Then as each rule in the Rule definition is examined, LF{2} should
+	// be identified.
+	//
+	// Rule1 <- 'Foo' LF 'Bar' Rule2
+	// Rule2 <- LF{2}
+	//
+	// Rules that have Text that matches RuleName, ClassName, or
+	// LitName should be used as is.
+	//
+	// Rule1    <- 'Foo' LF 'Bar' EndBlock
+	// EndBlock <- LF{2}
+	return ""
+}
 
 // ------------------------------ Errors ------------------------------
 
